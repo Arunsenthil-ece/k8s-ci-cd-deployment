3bf93b0d7feeb76d18de51d3a56552dfc3875a7a
Arunsenthil Jothiswarupan <arunsenthil.ece@gmail.com>
2026-03-22 17:36:23 +0530

    Adding health endpoint

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello from Kubernetes!"
 
+@app.route("/health")
+def health_check():
+    return {"status": "UP"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
